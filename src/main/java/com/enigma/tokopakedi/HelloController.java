d4c5f24f781420c5c7ef7e472d818f8feb39f460
saputra khotim <saputra.khotim108@gmail.com>
2023-12-20 13:22:59 +0700

    initial commit

---
+++ src/main/java/com/enigma/tokopakedi/HelloController.java
@@ -0,0 +1,66 @@
+package com.enigma.tokopakedi;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+@RestController
+public class HelloController {
+
+    private List<Map<String,Object>> users= List.of(
+            Map.of( "id",1,"name","Saputra"),
+            Map.of("id",2,"name","Khotim")
+    );
+
+    private List<String> fruits= List.of("Apple" , "Mangga" , "Mangga Muda", "Mangga Busuk", "Nanas", "Nanas Muda", "Nanas Busuk");
+
+    @GetMapping(path = "/")
+    public String helloWorld(){
+        return "<h1>Hello World</h1>";
+    }
+
+    @GetMapping(path = "/buah")
+    public List<String> fruits(){
+        return List.of("Apel","Melon","Semangka","Jeruk");
+    }
+
+    @GetMapping(path = "/cars")
+    public Map<String,Object> cars(){
+        return Map.of(
+                "brand","Toyota",
+                "name","Supra",
+                "year",2020
+        );
+    }
+
+    @GetMapping(path = "/users/{userId}")
+    public Map<String, Object> getUserById(@PathVariable Integer userId){
+        Map<String, Object> tempt= new HashMap<>();
+
+        for (Map<String, Object> user : users) {
+            if (user.get("id").equals(userId)){
+                tempt=user;
+            }
+        }
+        return tempt;
+    }
+
+
+    @GetMapping(path = "/fruits")
+    //localhost:8081/fruits?search=mangga
+    public List<String> getList(@RequestParam String search){
+        List<String> result = fruits.stream().filter(s -> s.toLowerCase()
+                .contains(search.toLowerCase()))
+                .toList();
+        return result;
+    }
+
+    @PostMapping(path = "/users")
+    public User postUser(@RequestBody User user){
+        return user;
+    }
+}
